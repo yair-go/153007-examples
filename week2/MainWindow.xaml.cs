803afce2b52e16d3c93c2a77fcb2aea84aa356eb
yairJCT <irigo1@gmail.com>
2018-10-14 15:33:38 +0300

    week 2

---
+++ week2/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace week2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+           
+            
+            InitializeComponent();
+        }
+
+        private void tb_Console_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void pb_Console_Click(object sender, RoutedEventArgs e)
+        {
+            Student s = new Student();
+            s.Name = "Cinderalla";
+            s.Age = 18;
+
+            tb_Console.Text = "Hello  " + s.ToString();
+
+        }
+
+        private void pb_GetNumber_Click(object sender, RoutedEventArgs e)
+        {
+            int cin = int.Parse(tb_Cin.Text);
+            int min = 0, max = 1000;
+            int[] arr = new int[100];
+            Random r = new Random();
+            for (int i = 0; i<arr.Length; i++)
+            {
+                arr[i] = r.Next(min, max);
+            }
+            StringBuilder nums = new StringBuilder();
+            foreach (int item in arr)
+            {
+                nums.Append(item);
+                nums.Append(", ");
+            }
+            tb_Console.Text = nums.ToString();
+           
+        }
+    }
+}
---
+++ week2/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2
+{
+    public class Student
+    {
+        #region Fields
+        private string _name;
+        private int _age;
+        private int _id;
+
+        #endregion
+
+        #region Ctors
+        public Student(string name = "", int age = 0, int id = 1)
+        {
+            Name = name;
+            Age = age;
+            _id = id;
+        }
+
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Age { get; set; }
+
+        public int Grade { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return _name + " " + Age ;
+        }
+        #endregion
+    }
+}
